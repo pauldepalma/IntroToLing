fb5bda79ca59f64bd38009ffcc1a6787fdc78866
Paul De Palma <depalma@gonzaga.edu>
2024-01-21 13:57:00 -0800

    programs 0 to 3

---
+++ ex2.py
@@ -0,0 +1,14 @@
+#ex3
+#Decision
+
+def main():
+    x = int(input("input an integer value for x\n"))
+    y = int(input("input an integer value for y\n"))
+    
+    if (x > y):
+        print ("x is largest")
+    else:
+        print ("y is largest")              
+main()
+
+   
---
+++ ex3.py
@@ -0,0 +1,16 @@
+#ex3
+#Loop
+
+def main():
+    bottom = int(input("Enter the first integer you want to see\n"))
+    top = int(input("Enter the last integer you want to see\n"))
+    print("Here they are")
+    print()
+    
+    cur = bottom;
+    while (bottom <= top):
+        print(bottom)
+        bottom = bottom + 1
+main()
+
+   
