b82322cbaea8e8d3bb85fddf81fcf1f6488de0a0
Paul De Palma <depalma@gonzaga.edu>
2024-01-28 18:28:06 -0800

    ex6

---
+++ ex4.py
@@ -0,0 +1,14 @@
+#This is a comment
+# ex4.py
+# decision
+
+def main():
+  var1 = int(input("Enter an integer\n"))
+  var2 = int(input("Enter another integer\n"))
+
+  if (var1 > var2):
+    print("The first number is greater than the second number")
+  else:
+    print("The second number is greater than the first number")
+
+main()
---
+++ ex6.py
@@ -0,0 +1,15 @@
+#This is a comment
+# ex5.py
+# decision within a loop
+
+def main():
+
+    stuff = []
+    for i in range(5):
+        stuff.append(i)
+
+    for i in range(5):
+        print(stuff[i])
+
+
+main()
